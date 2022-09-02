5910314cc23fc777b3cfeff1048281c9e5df5300
Mykhailo Pomorskyi <mykhailo.pomorskyi@sollers.eu>
2022-09-02 10:45:01 +0200

    Setting project and importing dependencies

---
+++ src/Components/componentsForPages/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import 'styles.css'
+
+const Header = () => {
+  return (
+    <div>
+        <div>logo</div>
+        <div>buttons</div>
+        <div>panel</div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Pages/FeedPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Header } from '../Components/componentsForPages'
+
+const FeedPage = () => {
+  return (
+    <Header></Header>
+    // <div>Feed</div>
+  )
+}
+
+export default FeedPage
